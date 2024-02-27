d2d01059b025ff205b716f0ee0d8c2e381168149
DanilShtefan <danilshtefan.66@gmail.com>
2024-02-27 12:13:40 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.slider__inner').slick({
+        dots:false,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        variableWidth:true,
+        prevArrow:false,
+        nextArrow:false,
+    })
+});
